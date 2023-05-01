0bd767b8ed6a3f25d0482a2bac3ad3a6a8ec4eb5
ELB0G <abourguiba510@gmail.com>
2023-05-01 02:47:03 +0100

    tbar9ich

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -157,7 +157,7 @@ class ResetPasswordController extends AbstractController
         }
 
         $email = (new TemplatedEmail())
-        ->from('abourguiba510@gmail.com')
+        ->from('aziz.abdouli@esprit.tn')
             ->to($user->getEmail())
             ->subject('Your password reset request')
             ->htmlTemplate('reset_password/email.html.twig')
